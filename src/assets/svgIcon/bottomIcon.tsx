374f44b3c9236a5a5e936ddb27162c587d0e56b3
Young Eric <erikbonopart12@gmail.com>
2025-03-19 14:47:07 +0300

    fix: -fix styles

--- src/assets/svgIcon/bottomIcon.tsx
+++ src/assets/svgIcon/bottomIcon.tsx
@@ -6,7 +6,7 @@ export const BottomIcon: React.FC<BottomIconProps> = ({className}) => {
     return (
         <svg
             className={className}
-            width="20" height="20" fill="#9e9e9e" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" stroke="#9e9e9e">
+            width="20" height="20" fill="#494949" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" stroke="#9e9e9e">
             <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
             <g id="SVGRepo_iconCarrier">
