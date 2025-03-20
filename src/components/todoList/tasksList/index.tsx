a3d3974b049fbe518ed0a5d4ac6f25b7551e228d
Young Eric <erikbonopart12@gmail.com>
2025-03-20 18:52:02 +0300

    refactor: -refactor styles

--- src/components/todoList/tasksList/index.tsx
+++ src/components/todoList/tasksList/index.tsx
@@ -1,13 +1,12 @@
 import React, {useCallback, useMemo} from 'react';
 import {observer} from "mobx-react-lite";
 import {useStores} from "@/providers";
-import { CustomUI } from '@/components';
+import {CustomUI} from '@/components';
 import crossIcon from "@/assets/images/cross.png";
 import {Todo} from "@/models-view";
 import {classNames} from "@/helpers";
 
 import "./index.scss"
-
 
 
 type TTasksListProps = {
@@ -17,28 +16,33 @@ type TTasksListProps = {
 }
 
 export const TasksList: React.FC<TTasksListProps> = observer(
-    ({userId,filteredTasks}) => {
+    ({userId, filteredTasks}) => {
         const {todoStore} = useStores();
         const userTasks = todoStore.getTodosByUserId(userId)
 
-         const deleteTask = useCallback(async (id: number): Promise<void> => {
-           await todoStore.deleteTodo(id)
+        const deleteTask = useCallback(async (id: number): Promise<void> => {
+            await todoStore.deleteTodo(id)
         }, [todoStore.deleteTodo])
 
-        const todos = useMemo( () => {
-          return  filteredTasks.map((todo) => {
-              const isCompleted = todo.completed;
-              const mods = {
-                  'completed-text': isCompleted,
-              };
+        const todos = useMemo(() => {
+            return filteredTasks.map((todo) => {
+                const isCompleted = todo.completed;
+                const mods = {
+                    'completed-text': isCompleted,
+                };
                 return (
                     <div className={"todo-container"} key={todo.id}>
-                        <CustomUI.CheckBox checked={isCompleted} onChange={() => todoStore.changeTodoStatus(todo.id) }/>
-                        <span className={classNames("",mods)}>
-                            {todo.title}
-                        </span>
+                        <div className={"todo-container__title"}>
+                            <CustomUI.CheckBox checked={isCompleted}
+                                               onChange={() => todoStore.changeTodoStatus(todo.id)}/>
+                            <span className={classNames("", mods)}>
+                                    {todo.title}
+                            </span>
+                        </div>
+
                         <button className={"delete-icon-container"}>
-                            <img src={crossIcon} alt="Delete" className={"delete-icon"} onClick={(e) => deleteTask(todo.id)}/>
+                            <img src={crossIcon} alt="Delete" className={"delete-icon"}
+                                 onClick={(e) => deleteTask(todo.id)}/>
                         </button>
                     </div>
                 )
